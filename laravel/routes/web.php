94864e16ec588ad9983f23024c4a131f1a7df70c
João Vitor Novais <joaoeuno2011@gmail.com>
2020-06-21 16:10:35 +0000

    adicionando bd e visualização de usuarios

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect('usuarios');
 });
 Route:: get('usuarios', 'UserController@listarUsuarios');
